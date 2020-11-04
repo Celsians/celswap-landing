6457df406b4cd1328d9ae8bbf534797975a85f88
Malisa Pusonja <malisa.pusonja@gmail.com>
2020-11-04 12:34:02 +0100

    Change parargaph.

--- index.php
+++ index.php
@@ -285,7 +285,7 @@
         </div>
         <div class="down-container">
             <div class="down-heading-box">
-                <h3>If you choose to earn in CEL tokens, inside of Celsius App, the rates increase <p style="font-size: 60px; margin-bottom: 23px;">up to 30%</p> based on the percentage of
+                <h3>If you choose to earn in CEL tokens, inside of Celsius App, the rates increase <p>up to 30%</p> based on the percentage of
                                         CEL tokens in your wallet</h3>
                 <p>This means that suddenly there can be people who have more CEL tokens than they want, simply because they can earn more with them. CelSwap is here to absorb those tokens, either by having them in LP or being swapped for some other token and vice versa. </p>
             </div>
